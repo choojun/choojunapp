701433d21ca18ae7a74824225b853ec8fdf12163
Tan Choo Jun <choojun@users.noreply.github.com>
2025-02-05 15:14:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,15 +14,3 @@ if st.button("Submit"):
     st.write(f"Hello, {name}! I love TAR UMT! ")
     
     
-# Insert a chat message container.
-with st.chat_message("user"):
-    st.write("Hello 👋")
-    st.line_chart(np.random.randn(30, 3))
-
-# Display a chat input widget at the bottom of the app.
->>> st.chat_input("Say something")
-
-# Display a chat input widget inline.
-with st.container():
-    st.chat_input("Say something")
-
